b361f51e1cce3ef9fc7a877570d883493ddb1c3f
Suntex Ray <abhikaboy@gmail.com>
2021-01-01 18:40:02 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+export class App extends React.Component(){
+  
+  render(){
+    return(
+      <p>hahah</p>
+    )
+  }
+}
